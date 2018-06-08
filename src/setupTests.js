947a40d1b66594e43baa383e18d1f82cf52492f5
Richard Samuels <richard.samuels@mongodb.com>
2018-06-08 11:15:49 -0400

    remove babel-polyfill from tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,5 +5,5 @@ import Adapter from 'enzyme-adapter-react-15';
 import Enzyme from 'enzyme';
 Enzyme.configure({ adapter: new Adapter() });
 
-import 'babel-polyfill';
+// Polyfills
 import 'url-search-params-polyfill';
