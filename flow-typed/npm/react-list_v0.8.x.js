2f920a9ca54bff4ff49bd310bcbe4ae1bcdf2ef3
Richard Samuels <richard.l.samuels@gmail.com>
2018-06-17 18:02:05 -0400

    Fix react-list flow libdef, add check to flow args

--- flow-typed/npm/react-list_v0.8.x.js
+++ flow-typed/npm/react-list_v0.8.x.js
@@ -1,20 +1,26 @@
 declare module 'react-list' {
-  import type { Element, Component, Ref } from 'react';
+  import type { Node, Element, Component, Ref } from 'react';
 
   declare export type ListType = 'simple' | 'variable' | 'uniform';
   declare export type Axis = 'x' | 'y';
 
+  declare export type Cache = {
+    [number]: number,
+  };
+
+  declare export type ItemsRendererFunction = (element: React$Node) => bool;
+
   declare export type Props = {
     axis?: Axis,
     initialIndex?: number,
-    itemRenderer?: Function,
-    itemSizeEstimator?: Function,
-    itemSizeGetter?: Function,
-    itemsRenderer?: Function,
+    itemRenderer?: (index: number, key: number) => React$Node,
+    itemSizeEstimator?: (index: number, cache: Cache) => number,
+    itemSizeGetter?: (index: number) => number,
+    itemsRenderer?: (items: React$Node[], ItemsRendererFunction) => React$Node,
     length?: number,
     minSize?: number,
     pageSize?: number,
-    scrollParentGetter?: <T>() => React$Ref<T>,
+    scrollParentGetter?: () => React$Node,
     threshold?: number,
     type?: ListType,
     useStaticSize?: bool,
