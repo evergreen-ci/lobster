f42482b401278dd1938b62f85339828faef5f80c
Michelle Chuang <michelle.chuang@mongodb.com>
2018-06-22 16:13:23 -0400

    Separated filters and highlights to different classes

--- src/components/Fetch/Filters.js
+++ src/components/Fetch/Filters.js
@@ -14,8 +14,6 @@ export const Filters = (props) => {
             removeFilter={props.removeFilter}
             toggleFilter={props.toggleFilter}
             toggleFilterInverse={props.toggleFilterInverse}
-            toggleFilterHighlight={props.toggleFilterHighlight}
-            toggleHighlightLine={props.toggleHighlightLine}
           />
         );
       })}
@@ -28,15 +26,11 @@ Filters.propTypes = {
   filters: PropTypes.arrayOf(PropTypes.shape({
     text: PropTypes.string.isRequired,
     on: PropTypes.bool.isRequired,
-    highlight: PropTypes.bool.isRequired,
-    highlightLine: PropTypes.bool.isRequired,
     inverse: PropTypes.bool.isRequired
   })).isRequired,
   removeFilter: PropTypes.func.isRequired,
   toggleFilter: PropTypes.func.isRequired,
-  toggleFilterInverse: PropTypes.func.isRequired,
-  toggleFilterHighlight: PropTypes.func.isRequired,
-  toggleHighlightLine: PropTypes.func.isRequired
+  toggleFilterInverse: PropTypes.func.isRequired
 };
 
 export class Filter extends React.PureComponent {
@@ -44,67 +38,24 @@ export class Filter extends React.PureComponent {
     filter: PropTypes.shape({
       text: PropTypes.string.isRequired,
       on: PropTypes.bool.isRequired,
-      highlight: PropTypes.bool.isRequired,
-      highlightLine: PropTypes.bool.isRequired,
       inverse: PropTypes.bool.isRequired
     }).isRequired,
     removeFilter: PropTypes.func.isRequired,
     toggleFilter: PropTypes.func.isRequired,
-    toggleFilterInverse: PropTypes.func.isRequired,
-    toggleFilterHighlight: PropTypes.func.isRequired,
-    toggleHighlightLine: PropTypes.func.isRequired
+    toggleFilterInverse: PropTypes.func.isRequired
   };
 
   removeFilter = () => this.props.removeFilter(this.props.filter.text);
   toggleFilter = () => this.props.toggleFilter(this.props.filter.text);
   toggleFilterInverse = () => this.props.toggleFilterInverse(this.props.filter.text);
-  toggleFilterHighlight = () => this.props.toggleFilterHighlight(this.props.filter.text);
-  toggleHighlightLine = () => this.props.toggleHighlightLine(this.props.filter.text);
-
-  checkWordDisable = () => {
-    if (this.props.filter.inverse) {
-      return (
-        <ToggleButton value={false} bsSize="small" bsStyle="primary" disabled>
-          word
-        </ToggleButton>
-      );
-    }
-    return (
-      <ToggleButton value={false} bsSize="small" bsStyle="primary">
-        word
-      </ToggleButton>
-    );
-  }
-
-  checkInverseDisable = () => {
-    if (this.props.filter.highlightLine) {
-      return (
-        <ToggleButton value={true} bsSize="small" bsStyle="success">
-          inverse
-        </ToggleButton>
-      );
-    }
-    return (
-      <ToggleButton value={true} bsSize="small" bsStyle="success" disabled>
-        inverse
-      </ToggleButton>
-    );
-  }
 
   render() {
-    // old buttons
-    /*
-
-      <Button className="filter-button-big" onClick={this.toggleFilter} bsStyle="warning" bsSize="xsmall">{this.props.filter.on ? 'filter off' : 'filter on'}</Button>
-      <Button className="filter-button-xbig" onClick={this.toggleFilterHighlight} bsStyle="info" bsSize="xsmall">{this.props.filter.highlight ? 'highlight off' : 'highlight on'}</Button>
-      <Button className="filter-button-big" onClick={this.toggleHighlightLine} bsStyle="primary" bsSize="xsmall">{this.props.filter.highlightLine ? 'words' : 'lines'}</Button>
-      <Button className="filter-button-big" onClick={this.toggleFilterInverse} bsStyle="success" bsSize="xsmall">{this.props.filter.inverse ? 'match' : 'inverse'}</Button>
-    */
     const buttonStyles = { marginLeft: '12px'};
     const lineStyles = { marginBottom: '5px'};
     return (
       <div style={lineStyles}>
         <Button className="filter-button" onClick={this.removeFilter} bsStyle="danger" bsSize="xsmall">{'\u2715'}</Button>
+        <span className="filter-label-text">Filter Options</span>
         <ToggleButtonGroup
           style={buttonStyles}
           type="radio"
@@ -113,37 +64,11 @@ export class Filter extends React.PureComponent {
           onChange={this.toggleFilter}
         >
           <ToggleButton value={true} bsSize="small" bsStyle="warning">
-            filter on
+              filter on
           </ToggleButton>
           <ToggleButton value={false} bsSize="small" bsStyle="warning">
-            filter off
+              filter off
           </ToggleButton>
-        </ToggleButtonGroup>
-        <ToggleButtonGroup
-          style={buttonStyles}
-          type="radio"
-          name="filter-highlight"
-          value={this.props.filter.highlight}
-          onChange={this.toggleFilterHighlight}
-        >
-          <ToggleButton value={true} bsSize="small" bsStyle="info">
-            highlight on
-          </ToggleButton>
-          <ToggleButton value={false} bsSize="small" bsStyle="info">
-            highlight off
-          </ToggleButton>
-        </ToggleButtonGroup>
-        <ToggleButtonGroup
-          style={buttonStyles}
-          type="radio"
-          name="filter-line-word"
-          value={this.props.filter.highlightLine}
-          onChange={this.toggleHighlightLine}
-        >
-          <ToggleButton value={true} bsSize="small" bsStyle="primary">
-            line
-          </ToggleButton>
-          {this.checkWordDisable()}
         </ToggleButtonGroup>
         <ToggleButtonGroup
           style={buttonStyles}
@@ -152,9 +77,11 @@ export class Filter extends React.PureComponent {
           value={this.props.filter.inverse}
           onChange={this.toggleFilterInverse}
         >
-          {this.checkInverseDisable()}
           <ToggleButton value={false} bsSize="small" bsStyle="success">
             match
+          </ToggleButton>
+          <ToggleButton value={true} bsSize="small" bsStyle="success">
+            inverse
           </ToggleButton>
         </ToggleButtonGroup>
         <span className="filter-text">{this.props.filter.text}</span>
---
+++ src/components/Fetch/Highlights.js
@@ -0,0 +1,91 @@
+import Button from 'react-bootstrap/lib/Button';
+import React from 'react';
+import PropTypes from 'prop-types';
+import {ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
+
+export const Highlights = (props) => {
+  return (
+    <div className="highlightBox">
+      <div className="highlight-box">{props.highlights.map(function(highlight) {
+        return (
+          <Highlight
+            key={JSON.stringify(highlight)}
+            highlight={highlight}
+            removeHighlight={props.removeHighlight}
+            toggleHighlight={props.toggleHighlight}
+            toggleHighlightLine={props.toggleHighlightLine}
+          />
+        );
+      })}
+      </div>
+    </div>
+  );
+};
+
+Highlights.propTypes = {
+  highlights: PropTypes.arrayOf(PropTypes.shape({
+    text: PropTypes.string.isRequired,
+    on: PropTypes.bool.isRequired,
+    line: PropTypes.bool.isRequired
+  })).isRequired,
+  removeHighlight: PropTypes.func.isRequired,
+  toggleHighlight: PropTypes.func.isRequired,
+  toggleHighlightLine: PropTypes.func.isRequired
+};
+
+export class Highlight extends React.PureComponent {
+  static propTypes = {
+    highlight: PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      on: PropTypes.bool.isRequired,
+      line: PropTypes.bool.isRequired
+    }).isRequired,
+    removeHighlight: PropTypes.func.isRequired,
+    toggleHighlight: PropTypes.func.isRequired,
+    toggleHighlightLine: PropTypes.func.isRequired
+  };
+
+  removeHighlight = () => this.props.removeHighlight(this.props.highlight.text);
+  toggleHighlight = () => this.props.toggleHighlight(this.props.highlight.text);
+  toggleHighlightLine = () => this.props.toggleHighlightLine(this.props.highlight.text);
+
+  render() {
+    const buttonStyles = { marginLeft: '12px'};
+    const lineStyles = { marginBottom: '5px'};
+    return (
+      <div style={lineStyles}>
+        <Button className="filter-button" onClick={this.removeHighlight} bsStyle="danger" bsSize="xsmall">{'\u2715'}</Button>
+        <span className="filter-label-text">Highlight Options</span>
+        <ToggleButtonGroup
+          style={buttonStyles}
+          type="radio"
+          name="highlight-on-off"
+          value={this.props.highlight.on}
+          onChange={this.toggleHighlight}
+        >
+          <ToggleButton value={true} bsSize="small" bsStyle="warning">
+              on
+          </ToggleButton>
+          <ToggleButton value={false} bsSize="small" bsStyle="warning">
+              off
+          </ToggleButton>
+        </ToggleButtonGroup>
+        <ToggleButtonGroup
+          style={buttonStyles}
+          type="radio"
+          name="highlight-line-word"
+          value={this.props.highlight.line}
+          onChange={this.toggleHighlightLine}
+        >
+          <ToggleButton value={true} bsSize="small" bsStyle="primary">
+            line
+          </ToggleButton>
+          <ToggleButton value={false} bsSize="small" bsStyle="primary">
+            word
+          </ToggleButton>
+        </ToggleButtonGroup>
+        <span className="filter-text">{this.props.highlight.text}</span>
+      </div>
+    );
+  }
+}
