0afb069cdf6d28ff8ca9ee0fbefdb83bde72e877
Richard Samuels <richard.samuels@mongodb.com>
2018-07-19 17:50:34 -0400

    Pre-deploy fix-ups

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { LinkContainer } from 'react-router-bootstrap';
 import './style.css';
 import About from '../About';
 import NotFound from '../NotFound';
@@ -14,29 +15,43 @@ import LogDrop from '../LogDrop';
 const logdrop = (props) => (<LogDrop {...props} />);
 const logviewer = (props) => (<Fetch {...props} />);
 const evergreenLogviewer = (props) => (<EvergreenLogViewer {...props} />);
+const about = (props) => (<About {...props} />);
+const notfound = (props) => (<NotFound {...props} />);
 
 const Main = () => (
-  <main>
+  <main className="lobster">
     <Switch>
-      <Route exact path="/lobster/about" component={About} />
+      <Route exact path="/lobster/about" render={about} />
       <Route path="/lobster/build/:build/test/:test" render={logviewer} />
       <Route path="/lobster/build/:build/all" render={logviewer} />
       <Route exact path="/lobster/evergreen/task/:id/:execution/:type" render={evergreenLogviewer} />
       <Route exact path="/lobster/evergreen/test/:id" render={evergreenLogviewer} />
       <Route path="/lobster/logdrop" render={logviewer} />
       <Route path="/lobster" render={logdrop} />
-      <Route path="*" component={NotFound} />
+      <Route path="*" render={notfound} />
     </Switch>
   </main>
 );
 
 // The Header creates links that can be used to navigate
 // between routes.
+const isActive = (match, location) => {
+  return location.pathname.startsWith('/lobster/') && location.pathname !== '/lobster/about';
+};
+
+const never = () => false;
+
 const Header = () => (
   <header className="head">
     <Nav bsStyle="pills">
-      <NavItem eventKey={1} href="/lobster/about">About</NavItem>
-      <NavItem eventKey={2} href="/lobster">Viewer</NavItem>
+      <LinkContainer to="/lobster/about" isActive={never}>
+        <NavItem>
+          <img alt="About" className="lobster-icon" src="/static/icons/lobster.ico" />
+        </NavItem>
+      </LinkContainer>
+      <LinkContainer to="/lobster" isActive={isActive}>
+        <NavItem>Viewer</NavItem>
+      </LinkContainer>
     </Nav>
   </header>
 );
