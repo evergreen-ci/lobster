5de227443af58fe29a27f04896c403a7c7727a04
Richard Samuels <richard.samuels@mongodb.com>
2018-07-24 13:24:27 -0400

    trap kill

--- .e2e.sh
+++ .e2e.sh
@@ -1,5 +1,7 @@
 npm run build
 node server --logs ./e2e > e2e_srv.log &
+trap "kill %1" EXIT
+
 sleep 5
 export LOBSTER_E2E_SERVER_PORT=$(cat e2e_srv.log | tail -n 1 | sed 's/App listening on 127.0.0.1:\([0-9]*\)!/\1/')
 echo $LOBSTER_E2E_SERVER_PORT
@@ -8,4 +10,3 @@ if [[ $CI -eq "true" ]]; then
 else
     npm run test:raw:e2e
 fi
-kill %1
