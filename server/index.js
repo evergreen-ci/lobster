a4871e6f4526095e8e334cd131b6ef5ac96086d2
Richard Samuels <richard.samuels@mongodb.com>
2018-07-23 20:24:39 -0400

    allow port 0

--- server/index.js
+++ server/index.js
@@ -3,7 +3,13 @@ const path = require('path');
 const fs = require('fs');
 const yargs = require('yargs');
 
-const PORT = process.env.PORT || 9000;
+const PORT = (() => {
+  const p = process.env.PORT || parseInt(yargs.argv.port, 10);
+  if (p === 0) {
+    return 0;
+  }
+  return 9000;
+})();
 
 const distPath = path.resolve(__dirname, '..', 'build', 'index.html');
 if (!fs.existsSync(distPath)) {
@@ -13,6 +19,7 @@ if (!fs.existsSync(distPath)) {
 
 const addr = yargs.argv.bind_address || '127.0.0.1';
 
-app.listen(PORT, addr, () => {
-  console.log('App listening on ' + addr + ':' + PORT + '!');
+const listener = app.listen(PORT, addr, () => {
+  const address = `${listener.address().address}:${listener.address().port}`;
+  console.log('App listening on ' + address + '!');
 });
