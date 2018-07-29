d2d8f9643569cbc146a9018f96fe6be5782de6fe
Richard Samuels <richard.samuels@mongodb.com>
2018-07-28 23:01:17 -0400

    cleanup side effects in server

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ const needle = require('needle');
 const bodyParser = require('body-parser');
 const hash = require('string-hash');
 
-const app = express();
+const dummyCache = require('./dummy_cache');
+const localCache = require('./local_cache');
+
 
 console.log('Starting server to support lobster log viewer.\nOptions:\n  --cache   Cache files after download in the provided directory. Note! All directory content will be deleted on the server start up! [optional]\n  --logs  An absolute path to log files that will be available to server [optional]\n  --bind_address  Specify the address lobster should bind to. Defaults to 127.0.0.1 [optional]\n  --port specify the port number. Defaults to 9000 [optional]');
 
@@ -16,139 +18,137 @@ function isValidURL(str) {
   return str.match(regex);
 }
 
-let myCache = require('./dummy_cache')();
-let logsDir = undefined;
-
-// Setup logger
-app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
-
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
-
-// Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
-
-app.use(function(err, req, res, next) {
-  if (res.headersSent) {
-    return next(err);
+function makeApp(logsPath, cache) {
+  const logsDir = logsPath === undefined ? undefined : path.resolve(logsPath);
+  if (logsDir) {
+    console.log('Serving local logs from directory: ' + logsDir);
   }
-  res.status(500);
-  res.render('error', { error: err });
-});
+  const myCache = cache === undefined ? dummyCache() : localCache(cache);
+  const app = express();
 
-// Always return the main index.html, so react-router render the route in the client
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
-});
+  // Setup logger
+  app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
-function cors(req, res) {
-  if (!req) return;
-  const origin = req.get('Origin');
-  if (origin) {
-    res.set({
-      'Access-Control-Allow-Origin': origin,
-      'Access-Control-Allow-Credentials': 'true',
-      'Access-Control-Allow-Headers': 'Content-Type',
-      'Access-Control-Allow-Methods': 'POST, OPTIONS'
-    });
-  }
-}
+  app.use(bodyParser.json()); // support json encoded bodies
+  app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
-app.options('/api/log', function(req, res, _next) {
-  cors(req, res);
-  res.send();
-});
+  // Serve static assets
+  app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
-app.post('/api/log', function(req, res, _next) {
-  cors(req, res);
+  app.use(function(err, req, res, next) {
+    if (res.headersSent) {
+      return next(err);
+    }
+    res.status(500);
+    res.render('error', { error: err });
+  });
 
-  const logUrl = req.body.url;
-  const filter = req.body.filter;
-  if (logUrl === undefined) {
-    console.log('url is undefined');
-    res.status(403).send('url cannot be undefined');
-    return;
-  }
-  console.log('url = ' + logUrl);
+  // Always return the main index.html, so react-router render the route in the client
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+  });
 
-  if (filter) {
-    console.log('filter = ' + filter);
-  } else {
-    console.log('filter is not set');
-  }
-
-  if (isValidURL(logUrl)) {
-    const fileName = hash(logUrl).toString();
-
-    myCache.get(fileName)
-      .then(data => {
-        console.log('got from cache: ' + fileName);
-        res.send(data);
-      })
-      .catch(_ => {
-        console.log(fileName + ' is not in the cache');
-
-        const stream = needle.get(logUrl);
-        let result = '';
-
-        stream.on('readable', function() {
-          for (let data = this.read(); data; data = this.read()) {
-            result += data;
-          }
-        });
-
-        stream.on('done', function(err) {
-          if (!err) {
-            console.log('done');
-            myCache.put(fileName, result)
-              .then(data => res.send(data));
-          } else {
-            console.log('Error: ' + err);
-          }
-        });
+  function cors(req, res) {
+    if (!req) return;
+    const origin = req.get('Origin');
+    if (origin) {
+      res.set({
+        'Access-Control-Allow-Origin': origin,
+        'Access-Control-Allow-Credentials': 'true',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'POST, OPTIONS'
       });
-  } else if (logsDir) {
-    // validate that the file is in logsDir
-    const reqPath = path.resolve(logsDir, logUrl);
-    if (!reqPath.startsWith(logsDir)) {
-      // since it's a security issue, we pretend it's not there
-      console.log(reqPath + ' not in log directory base (' + logsDir + ')');
-      res.status(404).send('log not found');
+    }
+  }
+
+  app.options('/api/log', function(req, res, _next) {
+    cors(req, res);
+    res.send();
+  });
+
+  app.post('/api/log', function(req, res, _next) {
+    cors(req, res);
+
+    const logUrl = req.body.url;
+    const filter = req.body.filter;
+    if (logUrl === undefined) {
+      console.log('url is undefined');
+      res.status(403).send('url cannot be undefined');
       return;
     }
+    console.log('url = ' + logUrl);
 
-    res.sendFile(reqPath, function(e) {
-      if (e && e.code === 'ENOENT') {
-        res.status(404).send('log not found').end();
-      } else {
-        res.status(500).send(e).end();
+    if (filter) {
+      console.log('filter = ' + filter);
+    } else {
+      console.log('filter is not set');
+    }
+
+    if (isValidURL(logUrl)) {
+      const fileName = hash(logUrl).toString();
+
+      myCache.get(fileName)
+        .then(data => {
+          console.log('got from cache: ' + fileName);
+          res.send(data);
+        })
+        .catch(_ => {
+          console.log(fileName + ' is not in the cache');
+
+          const stream = needle.get(logUrl);
+          let result = '';
+
+          stream.on('readable', function() {
+            for (let data = this.read(); data; data = this.read()) {
+              result += data;
+            }
+          });
+
+          stream.on('done', function(err) {
+            if (!err) {
+              console.log('done');
+              myCache.put(fileName, result)
+                .then(data => res.send(data));
+            } else {
+              console.log('Error: ' + err);
+            }
+          });
+        });
+    } else if (logsDir) {
+      // validate that the file is in logsDir
+      const reqPath = path.resolve(logsDir, logUrl);
+      if (!reqPath.startsWith(logsDir)) {
+        // since it's a security issue, we pretend it's not there
+        console.log(reqPath + ' not in log directory base (' + logsDir + ')');
+        res.status(404).send('log not found');
+        return;
       }
-    });
-  } else {
-    console.log('Must provide the --logs argument to handle local files');
-    res.status(400).end();
-  }
-});
+
+      res.sendFile(reqPath, function(e) {
+        if (e && e.code === 'ENOENT') {
+          res.status(404).send('log not found').end();
+        } else {
+          res.status(500).send(e).end();
+        }
+      });
+    } else {
+      console.log('Must provide the --logs argument to handle local files');
+      res.status(400).end();
+    }
+  });
+
+  return app;
+}
 
 const makeListener = (addr = '127.0.0.1', port = 9000, logsPath, cache, callback) => {
-  if (cache != null) {
-    myCache = require('./local_cache')(cache);
-  }
-  if (logsPath != null) {
-    const absPath = path.resolve(logsPath);
-    console.log('Serving local logs from directory: ' + absPath);
-    logsDir = absPath;
-  }
-
-  const listener = app.listen(port, addr, () => {
+  const listener = makeApp(logsPath, cache).listen(port, addr, () => {
     if (callback != null) {
       return callback(listener);
     }
   });
-  return;
 };
 
 module.exports = {
-  app: app,
+  makeApp: makeApp,
   makeListener: makeListener
 };
