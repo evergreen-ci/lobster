e530f5a9c2a87135bb8adf54d6d247001f0a6638
Richard Samuels <richard.l.samuels@gmail.com>
2018-07-30 19:15:49 -0400

    fix flow errors

--- src/reducers/logProcessor/resmokeTestEvents.spec.js
+++ src/reducers/logProcessor/resmokeTestEvents.spec.js
@@ -7,10 +7,16 @@ import fs from 'fs';
 
 describe('events', function() {
   test('sharding-migration-fail', function() {
-    console.log('testing events: sharding');
+    const inState = {
+      lines: [],
+      events: [],
+      identity: null,
+      isDone: false,
+      colorMap: {}
+    };
     const rawFilePath = path.resolve('.') + '/src/reducers/logProcessor/shardingRaw.txt';
     const rawFile = fs.readFileSync(rawFilePath).toString();
-    const state = resmoke({}, rawFile);
+    const state = resmoke(inState, rawFile);
     const comparisonPath = path.resolve('.' + '/src/reducers/logProcessor/sharding.json');
     const comparisonFile = fs.readFileSync(comparisonPath, 'utf8');
     const comparisonJSON = JSON.parse(comparisonFile);
@@ -19,10 +25,16 @@ describe('events', function() {
   });
 
   test('validate-collections', function() {
-    console.log('testing events: validate collections');
+    const inState = {
+      lines: [],
+      events: [],
+      identity: null,
+      isDone: false,
+      colorMap: {}
+    };
     const rawFilePath = path.resolve('.') + '/src/reducers/logProcessor/validateCollectionsRaw.txt';
     const rawFile = fs.readFileSync(rawFilePath).toString();
-    const state = resmoke({}, rawFile);
+    const state = resmoke(inState, rawFile);
     const validateCollectionsPath = path.resolve('.' + '/src/reducers/logProcessor/validateCollections.json');
     const validateCollectionsFile = fs.readFileSync(validateCollectionsPath, 'utf8');
     const validateCollectionsJSON = JSON.parse(validateCollectionsFile);
