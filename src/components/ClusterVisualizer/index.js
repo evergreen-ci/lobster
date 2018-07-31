650c3fcc34dbbc0063d98ce7b26a9c75721d101d
Michelle Chuang <michelle.chuang@mongodb.com>
2018-07-31 16:39:27 -0400

    Added vegaTooltip

--- src/components/ClusterVisualizer/index.js
+++ src/components/ClusterVisualizer/index.js
@@ -5,6 +5,7 @@ import type { Node as ReactNode } from 'react';
 import { connect } from 'react-redux';
 import { type Event } from '../../models';
 import vegaEmbed from 'vega-embed'; // vegaEmbed.embed(…)
+import vegaTooltip from 'vega-tooltip';
 import jQuery from 'jquery';
 
 type Props = {|
@@ -24,32 +25,10 @@ export class ClusterVisualizer extends React.PureComponent<Props, State> {
     super(props);
     this.state = {
       opts: {
-        actions: { source: false, editor: false, export: false, compiled: false },
-        tooltip: {
-          showAllFields: true,
-          fields: [
-            {
-              'field': 'start',
-              'formatType': 'time',
-              'format': '%H:%M:%S.%L'
-            },
-            {
-              'field': 'end',
-              'formatType': 'time',
-              'format': '%H:%M:%S.%L'
-            },
-            {
-              'field': 'log_start',
-              'formatType': 'time',
-              'format': '%H:%M:%S.%L'
-            },
-            {
-              'field': 'log_end',
-              'formatType': 'time',
-              'format': '%H:%M:%S.%L'
-            }
-          ]
-        }
+        actions: { source: false, editor: false, export: false, compiled: false }
+      },
+      tooltipsOptions: {
+        sanitize: (value) => 'hello' + String(value)
       },
       views: [],
       spec: {}
@@ -70,6 +49,12 @@ export class ClusterVisualizer extends React.PureComponent<Props, State> {
     this.baseDiv = div;
   }
 
+  addToViews = (view) => {
+    const newViews = this.state.views.slice();
+    newViews.push(view);
+    this.setState({ views: newViews });
+  }
+
   withSpec = (filename: string, events: Event[]) => {
     console.log('withspec');
     jQuery.getJSON('/' + filename, (data) => {
@@ -83,8 +68,10 @@ export class ClusterVisualizer extends React.PureComponent<Props, State> {
 
   graph = (): ?ReactNode => {
     if (this.baseDiv != null) {
-      vegaEmbed('#clusterVis', this.state.spec, this.state.opts).then(function() {
+      vegaEmbed('#clusterVis', this.state.spec, this.state.opts).then((result) => {
         console.log('vega embed success!');
+        this.addToViews(result.view);
+        vegaTooltip(result.view, this.state.tooltipsOptions);
       });
     }
   }
