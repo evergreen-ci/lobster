0e886090f4bb628438c29430c501afcb468ea771
Domino Weir <domino.weir@mongodb.com>
2020-03-10 16:56:25 -0400

    add more complex test

--- src/components/LogView/LogLineText.spec.js
+++ src/components/LogView/LogLineText.spec.js
@@ -3,7 +3,9 @@ import Enzyme from 'enzyme';
 import LogLineText from './LogLineText';
 
 describe('EvergreenLogViewer', () => {
-  const simpleJson = '{"std_get_0_envDataEntry":"distmod","std_get_1_envDataEntry":"rhel62"}';
+  const simpleText = '{"std_get_0_envDataEntry":"distmod","std_get_1_envDataEntry":"rhel62"}';
+  const complexText = '[js_test:backup_restore_rolling] 2020-03-02T08:52:04.781+0000 d20521| {"t":{"$date":"2020-03-02T08:52:04.780+0000"},"s":"I", "c":"RECOVERY","id":23987,"ctx":"initandlisten","msg":"WiredTiger recoveryTimestamp. Ts: {recoveryTimestamp}","attr":{"recoveryTimestamp":{"$timestamp":{"t":0,"i":0}}}}and then some more text'
+
   test('pretty-print-test', () => {
     const wrapper = Enzyme.shallow(
       <LogLineText
@@ -21,9 +23,12 @@ describe('EvergreenLogViewer', () => {
         prettyPrint={true}
       />);
 
-      const simpleJsonActual = wrapper.instance().findJSONObjectsInLine(simpleJson);
-      const simpleJsonExpected = [JSON.stringify(JSON.parse(simpleJson), null, 2).replace(/"([^"]+)":/g, '$1:')];
+      const simpleTextExpected = [JSON.stringify(JSON.parse(simpleText), null, 2).replace(/"([^"]+)":/g, '$1:')];
+      const simpleTextActual = wrapper.instance().findJSONObjectsInLine(simpleText);
 
-      expect(simpleJsonActual).toEqual(simpleJsonExpected);
+      const complexTextExpected = ['[js_test:backup_restore_rolling] 2020-03-02T08:52:04.781+0000 d20521| ', JSON.stringify(JSON.parse(complexText.substring(70, 293)), null, 2).replace(/"([^"]+)":/g, '$1:'), 'and then some more text'];
+      const complexTextActual = wrapper.instance().findJSONObjectsInLine(complexText);
+
+      expect(simpleTextActual).toEqual(simpleTextExpected);
   });
 });
