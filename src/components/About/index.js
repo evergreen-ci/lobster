ae1427fcb5319007153578e112f95a33bbf6d860
Maria van Keulen <mvankeulen94@gmail.com>
2021-03-10 13:43:51 -0500

    Update index.js

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -10,13 +10,13 @@ const About = () => (
     </p>
     <p> Lobster can: </p>
     <ul>
-      <li>search any bookmarked or filtered lines by regexp (the &quot;Find&quot; button). If the regexp finds multiple occurrences in the same log line, it will only count one, but it will highlight them all. As of now, it will also not scroll to the right if the matching text is off screen.</li>
+      <li>search any bookmarked, filtered, or otherwise visible lines by regexp (the &quot;Find&quot; button). If the regexp finds multiple occurrences in the same log line, it will only count one, but it will highlight them all. As of now, it will also not scroll to the right if the matching text is off screen.</li>
       <li>apply one or more regexp filters to the log lines (the &quot;Add Filter&quot; button)</li>
       <li>enable and disable filters</li>
       <li>match or inverse match filters</li>
       <li>cache the recently accessed files locally to improve load time. It can be set with the --cache if running Lobster locally</li>
       <li>view locally-stored log files. If running Lobster locally, the option --logs sets the absolute path that is available to the server for read server command line argument</li>
-      <li>double click on a line number to bookmark (or unbookmark) that line and click on that number on the left-hand side to jump to it</li>
+      <li>double click on a line to bookmark (or unbookmark) that line and click on that line number on the left-hand side to jump to it</li>
       <li>click on the &apos;Wrap&apos; toggle to turn line wrapping on and off</li>
       <li>pre-format all bookmarked lines for display in JIRA. The JIRA text area contains the formatted bookmark content</li>
       <li>...and more!</li>
