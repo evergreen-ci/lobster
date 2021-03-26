f6352491689060f79d17cc33f76a147c87f90d6a
Mohamed <mohamed.khelif@mongodb.com>
2021-03-26 18:31:55 -0400

    Change env variable for prod

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // @flow strict
 
-export const isProd: boolean = process.env.NODE_ENV === "production";
+export const isProd: boolean = process.env.NODE_ENV === 'production';
 export const LOGKEEPER_BASE: string =
-  process.env.REACT_APP_LOGKEEPER_BASE || "http://logkeeper.mongodb.org";
+  process.env.REACT_APP_LOGKEEPER_BASE || 'https://logkeeper.mongodb.org';
 export const EVERGREEN_BASE: string =
-  process.env.REACT_APP_EVERGREEN_BASE || "https://evergreen.mongodb.com";
+  process.env.REACT_APP_EVERGREEN_BASE || 'https://evergreen.mongodb.com';
