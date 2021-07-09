056cd66aa708431561cfb6808dff15ab747136bc
SupaJoon <arjun.patel@mongodb.com>
2021-07-09 13:40:32 -0400

    render job logs button for some evg test logs

--- src/components/Fetch/CollapseMenu.js
+++ src/components/Fetch/CollapseMenu.js
@@ -3,7 +3,7 @@
 import React from "react";
 import type { Node as ReactNode } from "react";
 import { getTestMetadata, type TestMetadata } from "../../util";
-import { EVERGREEN_BASE, LOGKEEPER_BASE } from "../../config";
+import { EVERGREEN_BASE, LOGKEEPER_BASE, SPRUCE_BASE } from "../../config";
 import "./style.css";
 import {
   Button,
@@ -187,8 +187,18 @@ function showDetailButtons(
       ]
     );
   } else if (id.type === "evergreen-test") {
-    const { logs } = testMetadata || {};
+    const { logs, group_id, execution, task_id } = testMetadata || {};
+
     const { url_html_display, url_raw_display } = logs || {};
+    if (Number.isFinite(execution) && group_id && task_id) {
+      buttons.push(
+        <Col key={0} lg={1}>
+          <Button style={col0Style} href={`${SPRUCE_BASE}/job-logs/${task_id}/${execution}/${group_id}`}>
+            Job Logs
+          </Button>
+        </Col>
+      )
+    }
     if (url_html_display && url_raw_display) {
       buttons.push(
         ...[
