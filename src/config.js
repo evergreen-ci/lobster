23242d79f0acd6e039447d9def64c0455ade3d38
Mohamed Khelif <mohamed.khelif@mongodb.com>
2022-03-15 16:09:09 -0400

    EVG-16537 Default to empty url for prod builds so links use relative domain (#126)

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const isProd: boolean = process.env.NODE_ENV === 'production';
 export const LOGKEEPER_BASE: string =
   process.env.REACT_APP_LOGKEEPER_BASE || 'https://logkeeper.mongodb.org';
 export const EVERGREEN_BASE: string =
-  process.env.REACT_APP_EVERGREEN_BASE || 'https://evergreen.mongodb.com';
+  process.env.REACT_APP_EVERGREEN_BASE || '';
  export const SPRUCE_BASE: string =
   process.env.REACT_APP_SPRUCE_BASE || 'https://spruce.mongodb.com';
 
